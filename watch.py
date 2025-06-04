20c72419e26bd312ef8e22c40b886b1b394b237b
Osakpolor Ogieriakhi <osakpolorogieriakhi@gmail.com>
2025-06-04 22:06:42 +0100

    Added this file

---
+++ watch.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    # match the embedded url pattern in the argument parsed, using regex 
+    matches = re.search(r'(?:<iframe)? src="https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9]+)(?:></iframe>)?"',s, re.IGNORECASE)
+
+    # check if matches were found
+    if matches:
+        # extract just the last part of the value of the embedded src attribute
+        video_id = matches.group(1)
+        # then return the shortened youtu.be link
+        return f"https://youtu.be/{video_id}"
+    # if no match
+    else:
+        # return None
+        return None
+
+# call the main function only when run in the terminal
+if __name__ == "__main__":
+    main()
